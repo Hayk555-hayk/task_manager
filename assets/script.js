fe7f703432c13fc43ef76cc7579bb4721af2c47a
Hayk <ayk.khachatryan.1994@gmail.com>
2022-10-10 21:29:49 +0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+let task_line = document.querySelector('.task_wrapper');
+let generated_html = "";
+let active = 1;
+console.log(tasks.length)
+for(let i = 0; i < tasks.length; i++) {
+    generated_html += `
+    <div class="one_task ${i == active ? "active" : ""} ${tasks[i].task_status == true ? "done" : ""}">
+        <div class="title">${tasks[i].title}</div>
+        <div class="description">${tasks[i].description}</div>
+    </div>`;
+}
+
+task_line.innerHTML = generated_html;
+
+let task_status = document.querySelector('.status');
+
+task_status.innerHTML = `${active}/${tasks.length}`;
