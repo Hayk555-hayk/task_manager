9b7ef5d1d2c9d201da1c9d567779badd811aa95f
Hayk <ayk.khachatryan.1994@gmail.com>
2022-11-02 01:20:01 +0400

    update

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -3,56 +3,49 @@ let tasks = [
         day_id: 1,
         day: "Понедельник",
         programming: 'Программирование Laravel: Surfside Media; lumen-Code Step By Step; orchid-agoalofalife (Видео 4, время 06:38)',
-        practice: 'Практика: Изучение конспектов по Laravel',
-        sport: "Спорт: гр-хлоп-ст-50, гр-кк.бц-50, гр-жм-лж-17, тр-лж-бк-50, тр-лж-50, тр-ст-50, пр-пл-20, пр-кст-20, !жм-обч-пр-скал-24",
+        sport: "Спорт: гр-хлоп-ст-50, гр-кк-бц-50, гр-жм-лж-17",
         religion: "Религия: Чтение приложения",
     },
     {
         day_id: 2,
         day: "Вторник",
-        programming: 'Программирование Vue Js: The net ninja-Vue JS 2 Tutorial; Vuex Tutorial; Vue CLI 3 Tutorial; Vuetify Tutorial; Tailwind CSS Tutorial; Vue.js 3 Tutorial; Vue 3 Animations & Transitions Tutorial; Vue 3 with TypeScript Jump Start',
-        practice: 'Практика: Изучение конспектов по Vue Js',
-        sport: "Спорт: бц-ст-30, бц-сд-30, бц-жм-12, нг-обч-12, нг-яг-12, нг-лж-яг-30, пр-скл-15, пр-нг-к-гр-15, !бц-жм-нг-обч-20",
-        english: "Английский: Изучения lingust",
+        programming: 'Программирование Vue Js: The net ninja-Vue JS 2 Tutorial; Vuex Tutorial; Vue CLI 3 Tutorial; Vuetify Tutorial; Tailwind CSS Tutorial; Vue.js 3 Tutorial; Vue 3 Animations & Transitions Tutorial; Vue 3 with TypeScript Jump Start; Владилен vue crm (Видео 5 время 02:58)',
+        sport: "Спорт: бц-ст-40, бц-сд-40, бц-жм-15",
+        english: "Английский: Изучения lingust (урок 4)",
     },
     {
         day_id: 3,
         day: "Среда",
         programming: 'Программирование Nuxt Js: Bitfumes',
-        practice: 'Практика: Изучение конспектов по Nuxt Js',
-        sport: "Спорт: пл-хл-вв-30, пл-рзв-ст-30, пл-жм-ст-30, сп-удр-30, сп-обч-30, сп-н-сб-30, пр-нг-15, пр-нг-к-рк-15, !жм-рк-к-пл-нг-вв-20",
+        sport: "Спорт: пл-хл-вв-30, пл-рзв-ст-30, пл-жм-ст-30",
         spain: "Испанский: Изучение lingust",
     },
     {
         day_id: 4,
         day: "Четверг",
-        programming: 'Программирование Laravel & Vue app: Amitav Roy-Restaurant manager - Laravel and Vuejs app; Video manager app with Laravel and Vuejs; Chat app with Vue.js and Laravel;',
-        practice: 'Практика: Изучение конспектов по Laravel & Vue app',
-        sport: "Спорт: гр-хлоп-ст-40, гр-кк.бц-40, гр-жм-лж-14, тр-лж-бк-40, тр-лж-40, тр-ст-40, пр-пл-17, пр-кст-17, !жм-обч-пр-скал-22",
+        programming: 'Программирование Laravel & Vue app: Amitav Roy-Restaurant manager - Laravel and Vuejs app; Video manager app with Laravel and Vuejs; Chat app with Vue.js and Laravel; Andrey Schemelun laravel and vue',
+        sport: "Спорт: тр-лж-бк-40, тр-лж-40, тр-ст-40",
         religion: "Религия: Чтение книги",
     },
     {
         day_id: 5,
         day: "Пятница",
         programming: 'Программирование Laravel & Nuxt: Tilly The Coder',
-        practice: 'Практика: Изучение конспектов по Laravel & Nuxt',
-        sport: "Спорт: бц-ст-30, бц-сд-30, бц-жм-12, нг-обч-12, нг-яг-12, нг-лж-яг-30, пр-скл-15, пр-нг-к-гр-15, !бц-жм-нг-обч-20",
+        sport: "Спорт: нг-обч-15, нг-яг-15, нг-лж-яг-30",
         english: "Английский: Создание словаря из 1000 слов",
     },
     {
         day_id: 6,
         day: "Суббота",
         programming: 'Программирование Microservices: Scalable Scripts',
-        practice: 'Практика: Изучение конспектов по Microservices',
-        sport: "Спорт: пл-хл-вв-30, пл-рзв-ст-30, пл-жм-ст-30, сп-удр-30, сп-обч-30, сп-н-сб-30, пр-нг-15, пр-нг-к-рк-15, !жм-рк-к-пл-нг-вв-20",
+        sport: "Спорт: сп-удр-30, сп-обч-30, сп-н-сб-30",
         spain: "Испанский: Создания словаря из 1000 слов",
     },
     {
         day_id: 0,
         day: "Воскресенье",
         programming: 'Программирование PHP & JS: php-Dani Krossing; js-oop The Net Ninja Object Oriented JavaScript; js-Владилен сложный js',
-        practice: 'Практика: Изучение конспектов по PHP & JS + чтение github',
-        sport: "Спорт: Бег до каскада, брссья-гр-7, брс-бц-7, тур-4, тур-бц-5, прс-обч-15, прс-брс-15",
+        sport: "Спорт: пр-пл-20, пр-обч-20, пр-нг-20, пр-склз-20",
         religion: "Религия: Написания письма",
     },
 
@@ -69,5 +62,16 @@ let lessons = [
         src: "./assets/lessons/laravel.html",
         img: "./assets/images/laravel.png"
     },
+    {
+        title: "VUE JS",
+        src: "./assets/lessons/vue.html",
+        img: "./assets/images/vue.png"
+    },
+    {
+        title: "ENGLISH",
+        src: "./assets/lessons/english.html",
+        img: "./assets/images/english.jpg"
+    },
+    
     
 ];
