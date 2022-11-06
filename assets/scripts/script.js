50bd8287da5e77020f72cebfe63526f8959a7cf4
Hayk <ayk.khachatryan.1994@gmail.com>
2022-11-06 20:17:45 +0400

    New updates

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,39 +1,5 @@
-let date_id = new Date();
-
-let tasks_html = "";
-
-for(let i = 0; i < tasks.length; i++) {
-    tasks_html += `
-    <div class="${tasks[i].day_id == date_id.getDay() ? 'day active' : 'day'}">
-        <div class="week_day">${tasks[i].day}</div>
-        <div class="list_of_tasks">
-            <div>${tasks[i].programming}</div>
-            <hr />
-            <div>${tasks[i].learned_part}</div>
-            <hr />
-            <div>${tasks[i].sport}</div>
-            <hr />`;
-            if(tasks[i].religion) {
-                tasks_html += `<div>${tasks[i].religion}</div>
-                <hr />`;
-            }
-            if(tasks[i].english) {
-                tasks_html += `<div>${tasks[i].english}</div>
-                <hr />`;
-            }
-            if(tasks[i].spain) {
-                tasks_html += `<div>${tasks[i].spain}</div>`;
-            }
-            tasks_html += `</div>
-        </div>`;
-}
-
-let task_element = document.querySelector('#tasks');
-
-task_element.innerHTML = tasks_html;
-
-
 let lessons_html = "";
+
 for(let i = 0; i < lessons.length; i++) {
     lessons_html += `
     <a href="${lessons[i].src}">
