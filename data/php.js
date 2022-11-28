d0d80c0d8ff229418984045d7c0303a422291c52
Hayk <ayk.khachatryan.1994@gmail.com>
2022-11-28 22:06:25 +0400

    php lesson

--- data/php.js
+++ data/php.js
@@ -1,4 +1,61 @@
 let php_data = [
-    '<div>php 1</div>',
-    '<div>php 2</div>'
+    `<div>(php)
+    Кроме index.php нужно создать файл .htaccess и внутри прописать 
+        <pre>
+        AddDefaultCharset utf-8
+        DirectoryIndex index.php
+        </pre>
+        php скрипты пишутся в тегах &lt?php ... ?&gt <br/>
+        Кодировка utf-8 нужна для правильного прописания русского языка 
+    </div>`,
+    `<div>(php)
+    Переменная это временная хранилище данных
+    <pre>
+    $name = 'Hayk';
+    $age = 28;
+    echo "hello my name is $name and my age is $age";
+    </pre>
+    В строке переменные можно выводить только двойными кавычками
+    <br/>
+    Чтобы вывести переменную и текст без пробелов можно использовать следующий тип написания
+    <pre>
+    echo "The traditions comes from {$age}th"
+    </pre>
+    Внедрять php в html очень легко 
+    <pre>
+    &ltdiv&gt some text &lt?php echo $name ?&gt some text &lt/div&gt
+    </pre>
+    Дата выводится при помощи функции date('y-m-d') <br/>
+    Константа в отличии от простой переменной неизменчива 
+    <pre>
+    define('SOME_CONST', 45);
+    const anodher_const = 35;
+    echo SOME_CONST;
+    define(CONST_ARRAY, [1,2,3]);
+    echo CONST_ARRAY[1];
+    </pre>
+    Есть и встроенные константы, их много, но вот пример нескольких из них
+    <pre>
+    __DIR__
+    __FILE__
+    __LINE__
+    PHP_VERSION 
+    </pre>
+    Кроме echo переменные можно вывести с помощью var_dump($arr), print($arr), print_r($arr)
+    </div>`,
+    `<div>(php)
+    Ошибки бывают трех типов: notice, warning и fatal. Иногда 1-й и 2-й тип ошибок выключен 
+    чтобы их видеть нужно прописать error_reporting(E_ALL); или ini_set('display_errors', 'on'); <br/>
+    Чтобы каждый раз не прописывать выщеуказанный код можно его прописать в файле php.ini в самом низу <br />
+    error_reporting=E_ALL
+    display_errors=On
+    </div>`,
+    `<div>(laravel)
+    Framework помогает быстрее разрабатывать, чтобы установить ларавел в первую очередь нужен компосер а потом нужно 
+    в командной строке прописать composer create-project laravel/laravel example-app, а для запуска сервера php artisan serve <br />
+    Laravel это mvc фруймворк (model-view-controller) <br />
+    Точкой входа для всех запросов к приложению Laravel является файл public/index.php <br />
+    Одним из наиболее важных поставщиков служб в вашем приложении является App-Providers-RouteServiceProvider.
+    Этот поставщик загружает файлы маршрутов, содержащиеся в каталоге routes приложения
+    </div>`
 ]
