237cea6e347aa8e453622bfb5ff62754a8a3f04a
Hayk <ayk.khachatryan.1994@gmail.com>
2022-12-02 08:41:32 +0400

    Laravel lesson add

--- data/php.js
+++ data/php.js
@@ -89,5 +89,187 @@ let php_data = [
     </div>`,
     `<div>(php)
     Метод header('Location: http://example.com'); выполнит редирект
+    </div>`,
+    `<div>(laravel)
+    Для установки проекта нужна команда composer create-project laravel/laravel example-app <br/>
+    В composer.json находятся зависимости проекта <br/>
+    Для начала проекта нужна команда php artisan serv </br>
+    Роутинг происходит в  web и api php файлах 
+    Пример роута
+    <pre>
+        Rpote::get('/path', [Controller::class, 'method'])->name('some name for this route');
+        Route::get('/path/{user_id}, [Controller::class, 'method])->name('route.name');
+        Route::get('/path/{user_id}, [Controller::class, 'method])-where('user_id, 'regular expression');
+    </pre>
+    В методe у контроллера такие аргументы принимаются как обычные аргументы
+    Пример такого метода 
+    <pre>
+        public function some_method($user_id) {
+            logic here ...
+        }
+    </pre>
+    Такие параметры могут быть необьязательными, where после route создаст критерии для переданного аргумента
+    <pre>
+        Route::get('/path/{user_id?}, [Controller::class, 'method])->where('user_id', 'some regular expression')
+        public function some_method($user_id = null) {
+            logic here ...
+        }
+    </pre>
+    Эти where могут быть и глобальными, в providers/RouteServiceProvider внутри метода booth
+    под всей логикой можем прописать 
+    <pre>
+        Route::pattern('user_id', 'some regular expression');
+    </pre>
+    Можно прописать route и для post и для get
+    <pre>
+        Route::match(['get', 'post'], '/path', [Controller::class, 'method'])
+        Route::any('/path', [Controller::class, 'method'])
+        // To get the requested method
+        public function some_method(Request $req) {
+           dd($req->method());
+        }
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    В контроллерах пишется вся логика приложения, для создания контроллера используем команду php artisan make:controller NameController <br/>
+    Общий вид по умолчанию в приложениях ларавел создается шаблонизатором blade они находятся в папке resources/views <br/>
+    <pre>
+        public function some_method($name) {
+            $user_name = $name;
+            return view('users', compact('user_name')); // but the file name is users.blade.php
+        }
+        //В blade Интерполяция происходит благодаря {{$user_name}}
+    </pre>
+    Создания blade компонента происходит при помощи php artisan make:component ComponentName команды
+    Команда создаст 2 файла, первый app/views/created php file, второй resources/views/components/blade файл <br />
+    Для вызова этого компонента в нужном blade пропишем ноый тег, через пропсы можно передавать данные x-componentName name="Hayk" :fruits="$fruits"
+    и для того чтобы этод name был виден нужно в php файле компонента прописать следующий команда
+    <pre>
+    class Header extends Component {
+        public $name;
+        public function __construct($name) {
+            $this->name = $name; // This name is available in Header blade as {{$name}}
+        }
+    }
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    Для создания контроллера нужна команда php artisan make:controller NameController, а команда php artisan serve запускает сервер <br/>
+    jsonplaceholder является хорошим местом для фейк api <br />
+    Создаем запрос на другой ресурс из нашего проекта 
+    <pre>
+    public function makeRequest() {
+        //GET EXAMPLE
+        $request = Http::get('url');
+        //POST EXAMPLE
+        $request = Http::post('url', [data]);
+        // PUT EXAMPLE
+        $request = Http::put('url', [data]);
+        //DELETE EXAMPLE
+        $request = Http::delete('url');
+        return $requrst->json();
+    }
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    Работа со строками 
+    <pre>
+    $str = Str::of('Welcome to tutorial')->after('Welcome to'); // will show tutoral
+    $str = Str::of('Welcome\ to\ tutorial')->afterLast('\\'); // will show tutoral
+    $str = Str::of('Welcome')->append(' to totorial'); // will show Welcom to tutoral
+    $str = Str::of('WELCOME')->lower(); // will show welcome (same for upper)
+    $str = Str::of('Welcome here')->replace('here', 'there'); // will show Welcome there
+    $str = Str::of('this is a title')->title(); // will show This Is A Titile
+    $str = Str::of('welcome to tutorial')->slug('-'); // will show welcome-to-tutorial
+    $str = Str::of('welcome here')->substr(8); // will show here
+    $str = Str::of('/laravel/')->trim('/'); // will show laravel
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    Обработать request нужно при помощи либо кустомных реквестов которые создаются командой php artisan make:request RequestName <br />
+    либо же использовать
+    <pre>
+    use Illuminate/Http/Request
+
+    public function Method(Request $request) {
+        ...
+        $request->method() // method of the request (get, post, etc)
+        $request->path() // url path of the request
+        $request->url() // full url path of the request
+        $request->fullUrl() //full url path of the request with get query params
+        $request->all() // all request data in array
+        $request->input('name') // get one input data from the form by the input name
+        ...
+    }
+    </pre>
+    </div>`, 
+    `<div>(laravel)
+    Каждая форма должна отправлять запрос к laravel при помощи csrf токена в blade есть директива @csrf <br />
+    В blade форме чтобы отправить запрос достатачно прописать  следующий код
+    <pre>
+        &ltform method="POST" action="{{route('route.name')}}"&gt
+        @csrf
+        ...all inputs of the form
+        &lt /form&gt
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    Выполнения валидации 
+    <pre>
+    public function Method(Request $request) {
+        $validData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:4|max:14'
+        ]);
+    }
+    </pre>
+    Сообщения об ошибках валидации пишутся в blade следующим образом 
+    <pre>
+    @error('mail')
+        {{$message}}
+    @enderror
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    В laravel есть resource контроллеры которые создают crud методы
+    <pre>
+    request type | request url              | controller method | url name 
+    get             projects                    index()             projects.index
+    get             projects/create             create()            projects.create
+    post            projects                    store()             projects.store
+    get             projects/{project}          show()              projects.show
+    get             projects/{project}/edit     edit()              projects.edit
+    put/patch       projects/{project}          update()            projects.update
+    delete          projects/{project}          destroy()           projects.destroy
+    </pre>
+    </div>`,
+    `<div>(laravel)
+    Хорошей практикой является всегда чтото ретурнить потому что все приложения ларавел находится в обертках которые постоянно 
+    чтото ретурнят 
+    </div>`,
+    `<div>(laravel)
+    Есть специальный роут для перенаправления Route::redirect('/here', '/there', 301);
+    Метод view принимает URI в качестве первого аргумента и имя шаблона в качестве второго аргумента. Кроме того, вы можете указать <br />
+    массив данных для передачи в шаблон в качестве необязательного третьего аргумента: Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+    </div>`,
+    `<div>(laravel)
+    Иногда может потребоваться указать параметр маршрута, который не всегда может присутствовать в URI. <br />
+    Вы можете сделать это, поставив знак ? после имени параметра. <br />
+    Не забудьте присвоить соответствующей переменной маршрута значение по умолчанию:
+    <pre>
+    Route::get('/user/{name?}', function ($name = null) {
+        return $name;
+    });
+    </pre>
+    Если вы хотите, чтобы параметр маршрута всегда ограничивался конкретным регулярным выражением, <br />
+    то вы можете использовать метод pattern. Вы должны определить эти шаблоны в методе boot вашего класса 
+    App-Providers-RouteServiceProvider:
+    <pre>
+    public function boot()
+    {
+        Route::pattern('id', '[0-9]+');
+    }
+    </pre>
     </div>`
+
 ]
