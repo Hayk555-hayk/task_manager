f5c90149698d240cde85ca17351841609673c34f
Hayk <ayk.khachatryan.1994@gmail.com>
2022-12-24 16:27:34 +0400

    node progress

--- data/data.js
+++ data/data.js
@@ -18,17 +18,17 @@ let tasks = [
     YouTube surfside media Урок 27 (осталось 1 видео)
     </span><br />
     <span class="small">
-    YouTube html lab mysql (урок 2), YouTube Andrey Schemelyun laravel vue, YouTube Аве кодер postgre, YouTube agoalofalife орхид, YouTube Гоша Дударь docker, YouTube coders tape all playlists, YouTube theurbanpenguin ubuntu, YouTube msft webcast windows, Documentation Laravel su урок 11, Documentation laravel demiart, YouTube learn linux tv centos, Documentation code mu php урок 9, Documentation code mu oop & mvc, Documentation Metanit SQL
+    YouTube html lab mysql (урок 2), YouTube Andrey Schemelyun laravel vue, YouTube Аве кодер postgre, YouTube agoalofalife орхид, YouTube Гоша Дударь docker, YouTube coders tape all playlists, YouTube theurbanpenguin ubuntu, YouTube msft webcast windows, Documentation Laravel su урок 11, Documentation laravel demiart, YouTube learn linux tv centos, Documentation code mu php урок 9, Documentation code mu oop & mvc, Documentation Metanit SQL, books php
     </span>`,
 
     `<span class="task_title">
     NODE NEST
     </span><br />
     <span class="task_description">
-    YouTube Илья Кантор node от а до я Урок 6
+    YouTube Илья Кантор node от а до я Урок 16
     </span><br />
     <span class="small">
-    YouTube #JS Code nest js Урок 1, YouTube gungner it mongodb, the net ninja json server and graph ql, Documentation Metanit node, Documentation Metanit mongodb, documentation docs nest com
+    YouTube #JS Code nest js Урок 1, YouTube gungner it mongodb, the net ninja json server and graph ql, Documentation Metanit node, Documentation Metanit mongodb, documentation docs nest com, books node
     </span>`,
 
     `<span class="task_title">
@@ -38,7 +38,7 @@ let tasks = [
     YouTube The net ninja all vue and nuxt tutorials (vue 2 tutorial урок 19)
     </span> <br />
     <span class="small">
-    YouTube bitfumes nuxt js, YouTube JavaScript ru git, the net ninja all js playlists, Владилен typescript Documentation Metanit
+    YouTube bitfumes nuxt js, YouTube JavaScript ru git, the net ninja all js and typescript playlists, Владилен typescript, Documentation vue2 vue3 typescript Metanit, books vue
     </span>`,
 
 //-------------------------------------------ЯЗЫКИ---------------------------------------------------------------------
--- data/node.js
+++ data/node.js
@@ -42,5 +42,104 @@ let node_data = [
 
         vasya.hello(petya);
     </pre>
+    </div>`,
+    `<div>(node)
+    Обьект модуль является переменной которая существует в каждом модуле, ее можно увидеть так console.log(module) <br />
+    Если модуль вызван из другого место то это можно проверить с if(module.parent) <br />
+    Все exports переменные и методы прописываются в module.exports по этому нет разници в export.user = user или this.user = user <br />
+    но лучше использовать export потому что this может потерять свой контекст <br />
+    Модуль фабрика позволяет передавать параметры модулю var logger = require('logger')(module) передаем текущий модуль <br />
+    А в logger пропишем следующий код
+    <pre>
+    module.exports = function(module) {
+        return function(some arguments) {
+            var args = [module.filename].concat([].slice.call(arguments))
+            console.log.apply(console, args); // Вывод логов каждого модуля 
+        }
+    }
+    </pre>
+    </div>`,
+    `<div>(node)
+    NPM для node как composer для php можно загружать зависимости <br />
+    Для создания своего пакета который возможно будет скачать из npm нужно прописать module.exports = function <br />
+    и создать package json где пропишем name и version пакета (npm init создаст package json) <br />
+    После создания пакета его нужно опубликовать npm publish но тут будет ошибка если не будет указан user <br />
+    npm adduser поможет добавить пользователя если его нет <br />
+    npm help покажет все команды npm <br />
+    npm s some title поиск в npm <br />
+    npm r packageName удаление пакета <br />
+    npm up обновление всех модулей, npm up packageName обновление указаного модуля <br />
+    Для установки нужной версии в npm i express@2 <br />
+    Модули можно устанавливать глобально при помощи флажка npm -g i express или для mak sudo npm -g i
+    </div>`,
+    `<div>(node)
+    Часто используемые модули 
+    <pre>
+    let util = require('util');
+    util.inspect(obj); // beautiful log for node, if the object has method, that method will be called
+    // console.log will call util automaticaly 
+    </pre>
+    Есть разница между console.log и console.warn - log стандартный поток вывода, warn поток ошибок (есть еще console.trace) <br />
+    Error.captureStackTrace(this) выводит местоположение ошибки <br />
+    EventEmitter основной обьект реализирующий работу с событиями в node js
+    <pre>
+    var EventEmitter = require('events').EventEmitter;
+
+    var server = new EventEmitter;
+
+    // request события выполнится по очереди 
+    server.on('request', function(request){
+        request.approved = true;
+    });
+    
+    server.on('request', function(request){
+        console.log(request);
+    });
+
+    server.emit('request', {from: 'client 1'});
+    server.emit('request', {from: 'client 2'});
+
+    server.on('error', function(err){
+        //
+    });
+
+    server.emit('errer', new Error('some error'));
+    </pre>
+    </div>`,
+    `<div>(node)
+    Создания сервера на node js
+    <pre>
+    //ordinary feature
+    var http = require('http');
+    var server == new http.Server(); // new Server and httpCreateServer are the same things
+
+    server.listen(3000, '127.0.0.1');
+
+    server.on('request', function(req, res) {
+        res.end('hello world');
+    });
+    -------------------------------------------------
+    // echo text by get request (127.0.0.1/echo?message=hello)
+    var http = require('http');
+    var url = require('url');
+
+    var server new http.Server(function(req,res) {
+        console.log(req.method);
+        console.log(req.url);
+        console.log(req.headers);
+
+        var urlParsed = url.parse(req.url, true); // true превратит message=hello в message: hello
+        console.log(urlParsed);
+
+        if(urlParsed.pathname === '/echo' && urlParsd.query.message) {
+            res.end(urlParsd.query.message);
+        } else {
+            res.statusCode = 404;
+            res.end('page not found');
+        }
+    });
+
+    server.listen(3000, '127.0.0.1');
+    </pre>
     </div>`
 ]
