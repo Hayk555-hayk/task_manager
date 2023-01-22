04987ec764a18dd04b0efae8de6f96247bfcfc03
Hayk <ayk.khachatryan.1994@gmail.com>
2023-01-22 19:35:26 +0400

    ruby progress

--- data/ruby.js
+++ data/ruby.js
@@ -104,5 +104,114 @@ let ruby_data = [
         <pre>
         rand max 100 // generating random number from 0 to 100
         </pre>
+    </div>`,
+    `<div>(ruby)
+    Геттеры и сеттеры
+    <pre>
+            class Item
+                def initialize // Эквивалент методу __construct в PHP
+                    @price = 30
+                end
+
+                def price // This is a getter function thath will return variable
+                    @someVariable // Getter variable 
+                end
+
+                def price=(value) // Setter function sign = must be written without space
+                    @someVariable = value
+                end
+
+            end
+
+            item = Item.new
+
+            item.price=10 // Setting a value 
+            p item.price // Getting a value
+        </pre>
+    </div>`,
+    `<div>(ruby)
+    attribute readers, writers and accessors
+    <pre>
+        class Item
+            attr_reader :price, :height // Short verssion of getter (code by one line)
+            attr_writer :price, :height // Short verssion of setter (again by one line)
+
+            attr_accessor :height, :price // This is more shorter, this is attr_reader and attr_writter in same time
+        end
+
+        item = Item.new
+        item.price = 40
+        puts item.price
+        </pre>
+    </div>`,
+    `<div>(ruby)
+    irb устонавливается вместе с ruby это php artisan tinker <br />
+    Обьекты в ruby называются хешами <br />
+    ID строк при вызове "string".object_id всегда будет разным, а ID символа :smt.object_id всегда один и тот же <br />
+    Конкатенация для символов не возможна 
+    <pre>
+    // Creating hash (object)
+    objectVariable = {
+        :name => 'Hayk' // => is an old syntacs 
+        age: 28 // new syntacs both :name and age: are symbols
+    }
+
+    p objectVariable[:name] // Outupting name key 
+
+    // Usage example
+    class Item 
+        def initialize(options)
+            @model = options[:model]
+            @price = options[:price]
+        end
+
+        attr_accessor :mode, :price
+    end
+
+    item1 = Item.new({model: 'phone', price: 454})
+    </pre>
+    </div>`,
+    `<div>(ruby)
+    Массивы в ruby
+    <pre>
+        array = ['w','x','y','z'] // New syntacs array = %w[w x y z]
+
+        array.push('c') // add c to the end of array
+        array.pop // delete array last element
+        array.shift // delete array first element
+
+        // Usage example
+        class Cart 
+        attr_reader :items
+
+            def initialize
+                @items = []
+            end
+
+            def add_item(item)
+                @items.push(item)
+            end
+
+            def remove_item
+                @items.pop
+            end
+        end
+
+        // Using Cart class in another file
+        require_relative 'cart'
+        cart = Cart.new
+    </pre>
+    </div>`,
+    `<div>(ruby)
+    Итерация массивов
+    <pre>
+    array = %w[q a z x s d 15]
+
+    array.each{ |letters| puts letters}
+
+    array.each do |letters|
+        puts letters if |letters| != 15
+    end
+    </pre>
     </div>`
 ]
