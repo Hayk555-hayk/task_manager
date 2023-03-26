0d093603c87e584a357497f567c70ee77a75a440
Hayk <ayk.khachatryan.1994@gmail.com>
2023-03-26 14:33:14 +0400

    task_manager v2

---
+++ assets/data/data.js
@@ -0,0 +1,74 @@
+const task_title = {
+    programming: ['Ruby', 'Ruby on Rails', 'PHP', 'Laravel', 'Node JS', 'Nest JS', 'JavaScript', 'React JS', 'Add techno'],
+    math: ['Algebra', 'Geometry', 'Physics'],
+    languages: ['English', 'German', 'Spanish', 'Japanese', 'Chinese', 'Hebrew'],
+    sport: ['Chest', 'Biceps', 'Shoulders', 'Triceps', 'Abs', 'Back', 'Legs'],
+    religion: ['Reading the Bible', 'Writing a letter', 'Reading the Bible through an app']
+}
+
+const notes = {
+    'Ruby': [
+        'Чтение кники Ruby (страница 1)'
+    ],
+    'Ruby on Rails': [
+        'Книга Rails стр(1)'
+    ],
+    'PHP': [
+        'Уроки PHP на code.mu (основной учебник - урок 1)'
+    ],
+    'Laravel': [
+        'Уроки Laravel на laravel.su (урок 1)'
+    ],
+    'Node JS': [
+        'Уроки Node js на metanit.com (урок 1)'
+    ],
+    'Nest JS': [
+        'Уроки по Nest JS на nestjs.ru (урок 1)'
+    ],
+    'JavaScript': [
+        'Уроки JavaScript на metanit.com (урок 1)'
+    ],
+    'React JS': [
+        'Уроки React js на metanit.com (урок 1)'
+    ],
+    'Add techno': [
+        'Уроки Graphql  на https://tproger.ru/translations/graphql-beginners-guide/ (урок 1)'
+    ],
+    'English': [
+        'lingust урок 1'
+    ] ,
+    'German': [
+        'lingust урок 1'
+    ] ,
+    'Spanish': [
+        'lingust урок 1'
+    ] ,
+    'Japanese': [
+        'lingust урок 1'
+    ] ,
+    'Chinese': [
+        'lingust урок 1'
+    ] ,
+    'Hebrew': [
+        'lingust урок 1'
+    ] ,
+    'Chest': ['Упражнения на грудь'],
+    'Biceps': ['Упражнения на бицепс'],
+    'Shoulders': ['Упражнения на плечи'],
+    'Triceps': ['Упражнения на трицепс'],
+    'Abs': ['Упражнения на пресс'],
+    'Back': ['Упражнения на спину'],
+    'Legs': ['Упражнения на ноги'],
+    'Reading the Bible': ['Reading the Bible'],
+    'Writing a letter': ['Writing a letter'],
+    'Reading the Bible through an app': ['Reading the Bible through an app'],
+    'Algebra': [
+        'Уроки по метематике 5 класс DaVinch YouTube'
+    ],
+    'Geometry': [
+        'Сначало нужно завершить математику 5, 6 классы'
+    ],
+    'Physics': [
+        'Сначало нужно завершить математику 5, 6 классы'
+    ]
+}
---
+++ assets/data/script.js
@@ -0,0 +1,51 @@
+function get_random_task(array) {
+    const random_index = Math.floor(Math.random() * array.length);
+    task = array[random_index]
+    return task
+}
+
+function get_tasks() {
+    let tasks = [];
+    tasks.push(get_random_task(task_title.religion));
+    tasks.push(get_random_task(task_title.programming));
+    tasks.push(get_random_task(task_title.math));
+    tasks.push(get_random_task(task_title.languages));
+    tasks.push(get_random_task(task_title.sport));
+    
+    let titles = ['Religion', 'Programming', 'Math', 'Languages', 'Sport'];
+    let page_content = document.querySelector('.page_content');
+
+    let page_content_new_html = '<div class="tasks">';
+    for(let i = 0; i < tasks.length; i++) {
+        page_content_new_html += `<div class='${tasks[i]}'><h3>${titles[i]}</h3>${tasks[i]}</div>`
+    }
+    page_content_new_html += '</div>';
+
+    page_content.innerHTML = page_content_new_html;
+}
+
+function show_notes(e) {
+    let note = document.querySelector('.notes');
+
+    note.innerHTML = '';
+    note.innerHTML = '<div>'
+
+    if (notes[e.target.className] != undefined) {
+        for(let i = 0; i < notes[e.target.className].length; i++) {
+            note.innerHTML += `<div>${notes[e.target.className]}</div>`
+        }
+    }
+    
+    note.innerHTML += '</div>'
+
+}
+
+function target_note(note) {
+    console.log(note);
+}
+
+const get_task = document.querySelector('.get_task');
+get_task.addEventListener('click', get_tasks);
+
+const target_notes = document.querySelector('.page_content');
+target_notes.addEventListener('click', show_notes);
