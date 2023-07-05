7c24928b06d3527c91afd7707332853201cfbc0a
Hayk <ayk.khachatryan.1994@gmail.com>
2023-07-05 15:05:38 +0400

    structural change

--- assets/data/script.js
+++ assets/data/script.js
@@ -1,29 +1,54 @@
+let date = new Date();
 
-function show_notes(e) {
-    let note = document.querySelector('.notes');
+const day_1 = document.getElementById("day_1");
+const day_2 = document.getElementById("day_2");
+const day_3 = document.getElementById("day_3");
+const card_title = document.getElementById("card_title");
+const day_img = document.getElementById('day_img');
 
-    let target = undefined;
 
-    if(e.target.value != undefined) {
-        target = e.target.value
-    }
+switch (date.getDay()) {
+    case 0:
+    case 3:
+        day_1.classList.add("active_day");
+        day_2.classList.add("inactive_day");
+        day_3.classList.add("inactive_day");
+        day_1.setAttribute('id', 'active_element');
+        card_title.innerText = "PHP, LARAVEL, VUE, RELIGION"
+        day_img.src = './assets/images/download.png'
+    break;
 
-    if (notes[target] != undefined) {
-        note.innerHTML = '';
-        note.innerHTML = '<div>';
+    case 1:
+    case 4:
+    case 6:
+        day_1.classList.add("inactive_day");
+        day_2.classList.add("active_day");
+        day_3.classList.add("inactive_day");
+        day_2.setAttribute('id', 'active_element');
+        card_title.innerText = "RUBY, RAILS, ANGULAR, SPORT"
+    break;
 
-        if (target == "Sport" || target == "Conspect") {
-            notes[target] = notes[target].sort(() => 0.5 - Math.random()).slice(0, 1);
-        }
-        
-        for(let i = 0; i < notes[target].length; i++) {
-            note.innerHTML += `<div>${notes[target][i]}</div>`
-        }
-        note.innerHTML += '</div>'
-    }
-    
-
+    case 2:
+    case 5:
+        day_1.classList.add("inactive_day");
+        day_2.classList.add("inactive_day");
+        day_3.classList.add("active_day");
+        day_3.setAttribute('id', 'active_element');
+        card_title.innerText = "NODE, NEST, REACT, DOG TRAINING"
+    break;
 }
 
-const target_notes = document.querySelector('#targeting_task');
-target_notes.addEventListener('change', show_notes);
+const active_element = document.getElementById("active_element");
+
+let clicks_count = 1
+const cart = document.getElementById("card");
+
+console.log(card)
+active_element.addEventListener('click', function() {
+    clicks_count += 1
+    if (clicks_count % 2 == 0) {
+        cart.style.opacity = '1'
+    } else {
+        cart.style.opacity = '0'
+    }
+  });
