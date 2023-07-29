110da017d31894b8b7f4eb7c98044f9cad01461b
Hayk <ayk.khachatryan.1994@gmail.com>
2023-07-29 22:13:00 +0400

    login register primary versions

--- assets/helper.js
+++ assets/helper.js
@@ -7,20 +7,31 @@ function generateUUID() {
     return uuid;
   }
 
-  function get_from_firebase(firebase_collection_name) {
+  function get_from_firebase(firebase_collection_name, query_type, sent_data) {
     let recived_date = []
-    db.collection(firebase_collection_name).get().then(data => {
-        data.docs.forEach(element => {
-            recived_date.push(element.data())
-        });
-    })
-
+    switch(query_type) {
+        case "all": 
+        db.collection(firebase_collection_name).get().then(data => {
+            data.docs.forEach(element => {
+                recived_date.push(element.data())
+            });
+        })
+        break;
+        case "login":
+            db.collection(firebase_collection_name).where('login', '==', sent_data.login).where('password', '==', sent_data.password).where('email', '==', sent_data.email).get().then(data => {
+                data.docs.forEach(element => {
+                    recived_date.push(element.data())
+                });
+            })
+        break;
+    }
+    
     return recived_date;
   }
 
   function insert_to_firebase(firebase_collection_name, uuid, data) {
     let created_data = []
-    db.connection(firebase_collection_name).doc(uuid).set(data).then(() => {
+    db.collection(firebase_collection_name).doc(uuid).set(data).then(() => {
         created_data.push(data)
     })
     return created_data;
---
+++ assets/script.js
@@ -0,0 +1,38 @@
+// Login 
+let login = document.getElementById('login')
+
+function login_to_task_manager() {
+  let user_login = document.getElementById('user_login').value;
+  let user_email = document.getElementById('user_email').value;
+  let user_password = document.getElementById('user_password').value;
+  let sent_data = {
+    login: user_login,
+    password: user_password,
+    email: user_email
+  }
+  console.log(user_login, user_email, user_password)
+  let check_user = get_from_firebase('users', 'login', sent_data)
+
+}
+
+login.addEventListener('click', login_to_task_manager)
+
+// Register
+let register = document.getElementById('register')
+
+function register_to_task_manager() {
+  let user_login = document.getElementById('user_login').value;
+  let user_email = document.getElementById('user_email').value;
+  let user_password = document.getElementById('user_password').value;
+  let sent_data = {
+    login: user_login,
+    password: user_password,
+    email: user_email
+  }
+  let uuid = generateUUID()
+
+  let register_user = insert_to_firebase('users', uuid, sent_data)
+
+}
+
+register.addEventListener('click', register_to_task_manager)
