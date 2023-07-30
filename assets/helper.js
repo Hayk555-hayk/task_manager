d464c2bdf52ed5f5860f641656e9c54cd4de7b02
Hayk <ayk.khachatryan.1994@gmail.com>
2023-07-30 10:57:35 +0400

    login register done

--- assets/helper.js
+++ assets/helper.js
@@ -1,3 +1,17 @@
+function check_user_existence() {
+  console.log(localStorage.getItem('userData'))
+  if (localStorage.getItem('userData') != null){
+    document.getElementById('unloged').style.display = 'none'
+    let user = JSON.parse(localStorage.getItem('userData'))
+    document.getElementById('user_data').innerText = `${user.login}`
+    document.getElementById('loged').style.display = 'block'
+
+  } else {
+    document.getElementById('loged').style.display = 'none'
+    document.getElementById('unloged').style.display = 'block'
+  }
+  return localStorage.getItem('userData');
+}
 function generateUUID() {
     let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       const r = (Math.random() * 16) | 0;
@@ -8,33 +22,58 @@ function generateUUID() {
   }
 
   function get_from_firebase(firebase_collection_name, query_type, sent_data) {
-    let recived_date = []
     switch(query_type) {
         case "all": 
         db.collection(firebase_collection_name).get().then(data => {
             data.docs.forEach(element => {
-                recived_date.push(element.data())
+                element.data()
             });
         })
         break;
         case "login":
             db.collection(firebase_collection_name).where('login', '==', sent_data.login).where('password', '==', sent_data.password).where('email', '==', sent_data.email).get().then(data => {
+              if (data.docs.length) {
                 data.docs.forEach(element => {
-                    recived_date.push(element.data())
+                  let user_data = element.data();
+                  user_data.id = element.id;
+                  console.log(user_data)
+                  localStorage.setItem('userData', JSON.stringify(user_data));
+                  check_user_existence()
                 });
+              } else {
+                console.log('no such user')
+              }
+              
             })
         break;
     }
     
-    return recived_date;
   }
 
-  function insert_to_firebase(firebase_collection_name, uuid, data) {
-    let created_data = []
-    db.collection(firebase_collection_name).doc(uuid).set(data).then(() => {
-        created_data.push(data)
-    })
-    return created_data;
+  function insert_to_firebase(firebase_collection_name, uuid, sent_data, registration = false) {
+    if (registration) {
+      db.collection(firebase_collection_name).where('login', '==', sent_data.login).where('password', '==', sent_data.password).where('email', '==', sent_data.email).get().then(data => {
+        if (data.docs.length) {
+          data.docs.forEach(element => {
+            console.log('user is existed')
+          });
+        } else {
+          console.log('user_created')
+          db.collection(firebase_collection_name).doc(uuid).set(sent_data).then(() => {
+              localStorage.setItem('userData', JSON.stringify(element.data()));
+              check_user_existence();
+          })    
+        }
+        
+      })
+    } else {
+      let created_data = []
+      db.collection(firebase_collection_name).doc(uuid).set(sent_data).then(() => {
+          created_data.push(sent_data)
+      })
+      return created_data;
+  
+    }
   }
 
   function remove_from_firebase(firebase_collection_name, uuid) {
--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,40 @@
+// Check user 
+check_user_existence();
 // Login 
 let login = document.getElementById('login')
 
 function login_to_task_manager() {
-  let user_login = document.getElementById('user_login').value;
-  let user_email = document.getElementById('user_email').value;
-  let user_password = document.getElementById('user_password').value;
   let sent_data = {
-    login: user_login,
-    password: user_password,
-    email: user_email
+    login: document.getElementById('user_login').value,
+    password: document.getElementById('user_password').value,
+    email: document.getElementById('user_email').value
   }
-  console.log(user_login, user_email, user_password)
-  let check_user = get_from_firebase('users', 'login', sent_data)
-
+  get_from_firebase('users', 'login', sent_data)
 }
 
 login.addEventListener('click', login_to_task_manager)
+
+// Log out
+let logout = document.getElementById('logout')
+
+function logout_from_task_manager() {
+  localStorage.removeItem('userData');
+  check_user_existence();
+}
+
+logout.addEventListener('click', logout_from_task_manager)
 
 // Register
 let register = document.getElementById('register')
 
 function register_to_task_manager() {
-  let user_login = document.getElementById('user_login').value;
-  let user_email = document.getElementById('user_email').value;
-  let user_password = document.getElementById('user_password').value;
   let sent_data = {
-    login: user_login,
-    password: user_password,
-    email: user_email
+    login: document.getElementById('user_login').value,
+    password: document.getElementById('user_password').value,
+    email: document.getElementById('user_email').value
   }
   let uuid = generateUUID()
-  let check_user = get_from_firebase('users', 'login', sent_data)
+  insert_to_firebase('users', uuid, sent_data, true)
 
 }
 
