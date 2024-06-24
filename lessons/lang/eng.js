813b827b606a90127519312af63a19dd8b5db8ab
Hayk <ayk.khachatryan.1994@gmail.com>
2024-06-24 08:35:16 +0400

    english

--- lessons/lang/eng.js
+++ lessons/lang/eng.js
@@ -1,5 +1,11 @@
 let engTitle = 'Английский';
 
 let engTutorial = `
-    <b>tutorial</b>
+    <b>Книга 1 стр 27</b>
+    pal - друг, pale - бледный, mop - швабра, mope - хандрить, pin - булавка, pine - сосна, tube - труба, jar - банка<br/>
+    Буква r после согласных не читается<br />
+    Cede - уступать, cad - хам, dim - тусклый, herd - стадо, gnat - комар, ink - чернила, mild - кроткй, knee - колено<br/>
+    Fork - вилка,molten - расплавленный, glove - перчатка, sulphur - сера, martyr - мученик, zany - шум<br/>
+    as a matter of fact - собственно говоря, as a result - в результате, at a glance - сразу с первого взгляда<br/>
+    in a hurry - второпях, it is a pity - жаль, to take a fancy to - увлекаться чемл.,проникнуться симпатией
 `
