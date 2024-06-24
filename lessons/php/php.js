82149ff1c55104b2c3463f71ec98b5df3714f16d
Hayk <ayk.khachatryan.1994@gmail.com>
2024-06-24 06:58:28 +0400

    Laravel mastering

--- lessons/php/php.js
+++ lessons/php/php.js
@@ -1,5 +1,14 @@
 let phpTitle = 'Mastering Laravel framework and PHP';
 
 let phpTutorial = `
-    <b> PHP tutorial</b>
+    <b><a href='https://www.coursera.org/learn/laravel-framework-and-php/lecture/EItS0/loops-and-functions-in-php'>Ссылка на урок</a></b><br/>
+    <b>PHP</b> это опен соурс язык скриптинга для программирования. 
+    Создан в 1994 году Расмусом Лердорфом. Каждая php программа начинается с php тега.
+    Метод phpinfo() покажет всю информацию про данный php язык<br/>
+    запустить обычный файл php можно так - <b>php -S localhost:8080</b>.
+    echo 'some tex' выводит текст в веб браузере<br/>
+    Переменные записываются через знак $ - $name = 'Hayk'. Синтаксис создания массива 
+    array('blue', 'red', 'green'); <br/>
+    Операторы +-/* имеют ту же логику что и в математике, пример условных операторов 
+    <img src="./lessons/php/php/1.png" />
 `
